469847783692e5a247494c3d4a6d1c2997491116
MANIKANTAPT <149669789+MANIKANTAPT@users.noreply.github.com>
2023-12-12 16:11:33 +0530

    Update App.java

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -6,7 +6,7 @@ package org;
  */
 public class App 
 {
-    public static void main( ab String[] args )
+    public static void main(  String[] args )
     {
         System.out.println( "Hello World!" );
     }
